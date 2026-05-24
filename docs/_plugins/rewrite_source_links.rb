6d56a1933180efb2c867aaf1350e61ed0aa44873
chen3feng <chen3feng@gmail.com>
2026-05-24 19:01:04 +0800

    Fix Jekyll plugin: remove nonexistent each_site_payload method

--- docs/_plugins/rewrite_source_links.rb
+++ docs/_plugins/rewrite_source_links.rb
@@ -6,28 +6,26 @@ Jekyll::Hooks.register :site, :post_render do |site|
   base = site.config.dig("source_rewrite", "base")
   next unless base
 
-  site.each_site_payload do |_|
-    site.pages.each do |page|
-      next unless page.output
+  site.pages.each do |page|
+    next unless page.output
 
-      page.output = page.output.gsub(%r{href="\.\./vllm/vllm/([^"]+)"}) do
-        "href=\"#{base}/vllm/#{$1}\""
-      end
-      page.output = page.output.gsub(%r{href="\.\./csrc/([^"]+)"}) do
-        "href=\"#{base}/csrc/#{$1}\""
-      end
-      page.output = page.output.gsub(%r{href="\.\./(setup\.py[^"]*)"}) do
-        "href=\"#{base}/#{$1}\""
-      end
-      page.output = page.output.gsub(%r{href="\.\./(CMakeLists\.txt[^"]*)"}) do
-        "href=\"#{base}/#{$1}\""
-      end
-      page.output = page.output.gsub(%r{href="\.\./(pyproject\.toml[^"]*)"}) do
-        "href=\"#{base}/#{$1}\""
-      end
-      page.output = page.output.gsub(%r{href="\.\./(cmake/[^"]+)"}) do
-        "href=\"#{base}/#{$1}\""
-      end
+    page.output = page.output.gsub(%r{href="\.\./vllm/vllm/([^"]+)"}) do
+      "href=\"#{base}/vllm/#{$1}\""
+    end
+    page.output = page.output.gsub(%r{href="\.\./csrc/([^"]+)"}) do
+      "href=\"#{base}/csrc/#{$1}\""
+    end
+    page.output = page.output.gsub(%r{href="\.\./(setup\.py[^"]*)"}) do
+      "href=\"#{base}/#{$1}\""
+    end
+    page.output = page.output.gsub(%r{href="\.\./(CMakeLists\.txt[^"]*)"}) do
+      "href=\"#{base}/#{$1}\""
+    end
+    page.output = page.output.gsub(%r{href="\.\./(pyproject\.toml[^"]*)"}) do
+      "href=\"#{base}/#{$1}\""
+    end
+    page.output = page.output.gsub(%r{href="\.\./(cmake/[^"]+)"}) do
+      "href=\"#{base}/#{$1}\""
     end
   end
 end
